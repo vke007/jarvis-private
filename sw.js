5fba7d5f747fd17eb93aa1ed2c5e2c9849e0a4b4
vke007 <161368166+vke007@users.noreply.github.com>
2026-02-26 00:30:48 +0530

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE='jarvis-v3';
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/'])));self.skipWaiting()});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
-self.addEventListener('fetch',e=>{if(e.request.url.includes('/api/'))return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)).catch(()=>caches.match('/')))});
